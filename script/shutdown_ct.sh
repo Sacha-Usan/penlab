5dd0c4a1234c7cdd8405445f07f62effbb046372
Sacha-Usan <42832326+Sacha-Usan@users.noreply.github.com>
2023-06-21 18:16:27 +0200

    Create shutdown_ct.sh

---
+++ script/shutdown_ct.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+pir=101
+pen=201
+
+nb=$(echo "$(wc -l /root/penlab/.container/CT/list | cut -d " " -f 1) / 2" | bc)
+
+for (( i=1; i<=$nb; i++ ))
+do
+  pct shutdown $pir
+  pct shutdown $pen
+
+  ((pir++))
+  ((pen++))
+done
